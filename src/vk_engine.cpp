cb5b37ba5eb80cd6fe0d7c91742cccb509fa4f31
eavraam <e_avraam@outlook.com>
2024-02-22 14:31:12 +0100

    Completed chapter 1.1, 1.2 - Vulkan Initialization

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -10,9 +10,15 @@
 #include <chrono>
 #include <thread>
 
+//bootstrap library
+#include "VkBootstrap.h"
+
+
 VulkanEngine* loadedEngine = nullptr;
 
 VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+constexpr bool bUseValidationLayers = false;
+
 void VulkanEngine::init()
 {
     // only one engine initialization is allowed with the application.
@@ -30,7 +36,16 @@ void VulkanEngine::init()
         SDL_WINDOWPOS_UNDEFINED,
         _windowExtent.width,
         _windowExtent.height,
-        window_flags);
+        window_flags
+    );
+
+    init_vulkan();
+
+    init_swapchain();
+
+    init_commands();
+
+    init_sync_structures();
 
     // everything went fine
     _isInitialized = true;
@@ -38,8 +53,17 @@ void VulkanEngine::init()
 
 void VulkanEngine::cleanup()
 {
+    // Objects have dependencies on each other, and we need to delete them in the correct order.
+    // Deleting them in the opposite order they were created is a good way of doing it.
     if (_isInitialized) {
 
+        destroy_swapchain();
+
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkDestroyDevice(_device, nullptr);
+
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+        vkDestroyInstance(_instance, nullptr);
         SDL_DestroyWindow(_window);
     }
 
@@ -73,6 +97,10 @@ void VulkanEngine::run()
                     stop_rendering = false;
                 }
             }
+
+            // Get specific keypress, exit on ESCAPE keypress
+            if (e.key.keysym.sym == SDLK_ESCAPE)
+                bQuit = true;
         }
 
         // do not draw if we are minimized
@@ -83,5 +111,106 @@ void VulkanEngine::run()
         }
 
         draw();
+    }
+}
+
+void VulkanEngine::init_vulkan()
+{
+    // create a builder from the bootstrap library
+    vkb::InstanceBuilder builder;
+
+    // make the vulkan instance, with basic debug features
+    auto inst_ret = builder.set_app_name("Example Vulkan Application")
+        .request_validation_layers(bUseValidationLayers) // for performance measurement, turn this off
+        .use_default_debug_messenger() // catches the log messages that validation layers output
+        .require_api_version(1, 3, 0)
+        .build();
+
+    // Get the value of previous statement and assert if it failed
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    // grab the instance
+    _instance = vkb_inst.instance;
+    _debug_messenger = vkb_inst.debug_messenger;
+
+    // get the surface of the window we opened with SDL
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+    // vulkan 1.3 features
+    VkPhysicalDeviceVulkan13Features features{};
+    features.dynamicRendering = true; // allows to skip renderpasses/framebuffers
+    features.synchronization2 = true; // upgraded version of synchronization functions
+
+    // vulkan 1.2 features
+    VkPhysicalDeviceVulkan12Features features12{};
+    features12.bufferDeviceAddress = true; // lets us use GPU pointers without binding buffers
+    features12.descriptorIndexing = true;  // gives us bindless textures
+
+    // use vkboostrap to select a GPU.
+    // we want a GPU that can write to the SDL surface and supports vulkan 1.3 with the correct features
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    vkb::PhysicalDevice physicalDevice = selector
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features)
+        .set_required_features_12(features12)
+        .set_surface(_surface)
+        .select()
+        .value();
+
+    // create the final vulkan device
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    // Get the VkDevice handle used in the rest of a vulkan application
+    _device = vkbDevice.device;
+    _chosenGPU = physicalDevice.physical_device;
+}
+
+void VulkanEngine::init_swapchain()
+{
+    create_swapchain(_windowExtent.width, _windowExtent.height);
+}
+
+void VulkanEngine::init_commands()
+{
+    // nothing yet
+}
+
+void VulkanEngine::init_sync_structures()
+{
+    // nothing yet
+}
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+    vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU, _device, _surface };
+
+    _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        // use default format selection
+        .set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+        // use vsync present mode
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+
+    _swapchainExtent = vkbSwapchain.extent;
+    // store swapchain and its related images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain()
+{
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    // destroy swapchain resources
+    for (int i = 0; i < _swapchainImageViews.size(); i++)
+    {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
     }
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -17,6 +17,21 @@ public:
 
 	static VulkanEngine& Get();
 
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+public:
+
 	//initializes everything in the engine
 	void init();
 
@@ -28,4 +43,13 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
